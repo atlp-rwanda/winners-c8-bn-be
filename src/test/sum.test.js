07135894b5417e9fe73f70f5d68ed97b67c69bad
nyakamwe <nyakamweaimable@gmail.com>
2022-05-23 13:25:54 +0200

    chore(coveralls): integrate coveralls
    -Integrating coveralls and coverage badge to ensure our codebase is tested properly which in turn provides quality assurance for the product
    
    [Coveralls #182209989]
    
    resolving conflict
    
    resolving conflicts

---
+++ src/test/sum.test.js
@@ -0,0 +1,10 @@
+import assert from 'assert'
+import {sum} from './sum'
+
+describe('coveralls testing', (done)=>{
+
+    it('makes sum of two numbers', ()=>{
+
+        assert(sum(2, 3) === 5)
+    })
+})
