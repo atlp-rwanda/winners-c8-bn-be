109d417026cfb0d5a59b66c43ba9cf4ba907ecfe
pacomesimon <smbonimpa2011@gmail.com>
2022-05-23 08:02:34 +0200

    HoundCI feedback response

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,39 +1,50 @@
-// This file was auto generated using the "sequelize-cli". the command used was: npx sequelize-cli init
+/* 
+    (comments by Pacome Simon)
+  - This file was auto generated using the "sequelize-cli". 
+    the command used was: npx sequelize-cli init.
+    The JS codes were commented, and can be used when necessary.
 
-'use strict';
+  - You can uncomment the JS codes below (starting from line 13), and use them.
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+    NB: please, remember to modify the code to use ES6 Syntax, 
+        and compile using Babel by running "npm run build"
+        (see package.json for more info) 
+*/
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// 'use strict';
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
+// const db = {};
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
 
-module.exports = db;
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
