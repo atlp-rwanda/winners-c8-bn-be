ccf9cfde9c371fe0288b4315061435c3d666d0d7
Ihimbazwe Elissa <ihimbazwelisa@gmail.com>
2022-06-01 13:20:13 +0200

    Added production mode to heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 //In .env use in development config env file
 //DATABASE_URL=postgres://username:password@localhost:5432/database_name
-//postgres://ihimbazwe:elissa123@localhost:5432/Development
+postgres://ihimbazwe:elissa123@localhost:5432/Development
 
 module.exports = {
 
@@ -29,5 +29,11 @@ module.exports = {
     port: process.env.PROD_DB_PORT,
     dialect: 'postgres',
     use_env_variable: "DATABASE_URL",
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: "postgres",
   },
 };
