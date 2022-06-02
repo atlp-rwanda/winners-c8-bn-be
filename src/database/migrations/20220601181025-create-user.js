f627b419e401f4a8cfe440308c24ad4a46badb0f
alicepretty <alicemuhorakeye15@gmail.com>
2022-06-02 22:57:48 +0200

    ft(user-register):user registration
    - create user registration route
    - test the endpoint
    - document the route
    
    [Delivers #1822100043]

---
+++ src/database/migrations/20220601181025-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+	 await queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.literal('uuid_generate_v4()'),
+			},
+			firstName: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			lastName: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true,
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			user_role: {
+				type: Sequelize.ENUM(
+					'admin',
+					'requester',
+					'manager',
+					'traveler',
+				),
+				defaultValue: 'requester',
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+
+		  
+				
+ 		 },
+		async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Users');
+ 		 }
+	};
--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-import "core-js/stable";
+import 'core-js/stable';
 import swaggerDocs from './docs';
 import swaggerUI from 'swagger-ui-express';
 import "regenerator-runtime/runtime";
 import DB from "./database/index";
 import express from "express";
 import routes from "./routes/index";
+import index from "./routes/welcome";
 import "dotenv/config";
 import getDefault from './helpers/getEnvironment'
 
-
-// connecting to database
-DB.authenticate()
-  .then(() => {
-    console.log("Database Connected");
-  })
-
-
 const PORT=getDefault(process.env.PORT, "5000")
 
+// const app = express();
 
 const app = express();
 
 // allow to parse json in body
 app.use(express.json());
 
-app.use("/api", routes);
+app.use('/api', routes);
+app.use('/', index);
+DB.authenticate()
+	.then(() => {
+		console.log('Database Connected');
+	})
+	.catch((err) => {
+		console.log('Database unable to connect', err);
+	});
 routes.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-app.get("/", async (req, res) => {
-  res.send({ message:'Hello World!' });
-});
 
 app.listen(PORT, () => {
   console.log("Server has started on port", PORT);
