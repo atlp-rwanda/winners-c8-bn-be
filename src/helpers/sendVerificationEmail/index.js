eb6e64466d78f4c355e01774520fa14f89190728
pacomesimon <smbonimpa2011@gmail.com>
2022-06-06 19:11:27 +0200

    feature(helpers):add verificationEmail function

---
+++ src/helpers/sendVerificationEmail/index.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+import sgMail from '@sendgrid/mail';
+import generateHTML from './generateHTML';
+
+const TECH_SUPPORT_EMAIL = process.env.TECH_SUPPORT___EMAIL_VERIFICATION;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY___EMAIL_VERIFICATION;
+const LOCAL_DOMAIN_NAME = process.env.LOCAL_DOMAIN_NAME___EMAIL_VERIFICATION || 'localhost'
+
+const sendVerificationEmail = (userEmail) => {
+    sgMail.setApiKey(SENDGRID_API_KEY);
+    const verificationLink = LOCAL_DOMAIN_NAME;
+    const msg = {
+        to: userEmail, // Change to your recipient
+        from: TECH_SUPPORT_EMAIL, // Change to your verified sender
+        subject: 'Congrats on your new Barefoot Normad account!',
+        text: `Congrats on your new Barefoot Normad account!
+                You can access our application by clicking Here: ${verificationLink}`,
+        html: generateHTML(TECH_SUPPORT_EMAIL,verificationLink),
+    }
+    sgMail
+    .send(msg)
+    .then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+
+export default sendVerificationEmail;
