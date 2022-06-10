b1c828afbc801e0d8b57f57bc7b0f4f8409a03d6
pacomesimon <smbonimpa2011@gmail.com>
2022-06-10 13:34:38 +0200

    chore(helper): clean the codes

--- src/helpers/sendVerificationEmail/example.js
+++ src/helpers/sendVerificationEmail/example.js
@@ -1,10 +1,5 @@
 import sendVerificationEmail from '../sendVerificationEmail'
 
-// (async ()=>{
-//     let output = await sendVerificationEmail('mbonimpa_218000124@stud.ur.ac.rw');
-//     console.log(output);
-// })();
-
 sendVerificationEmail('geekyrw@gmail.com')
         .then((output) => {  
             console.log(output);
