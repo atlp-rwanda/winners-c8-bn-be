1e386ea96cc9fb014a257e1323746686f5734749
alain-honore <himbazaalain022@gmail.com>
2022-06-10 16:05:45 +0200

    feat(Authentication): Generating JWT for user

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,5 +1,6 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
+
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
@@ -9,13 +10,30 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
-      name: {
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
         type: DataTypes.STRING,
         allowNull: false,
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull:true
+      },
+      isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      email_token: {
+        type: DataTypes.STRING,
+        defaultValue: false,
+        allowNull:true
       },
       user_role: {
         type: DataTypes.STRING,
@@ -30,3 +48,8 @@ module.exports = (sequelize, DataTypes) => {
   );
   return User;
 };
+
+
+
+
+ 
