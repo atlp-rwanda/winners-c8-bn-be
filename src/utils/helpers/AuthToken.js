68ee03df7d66d85f01890a3418f47ea306f662df
alain-honore <himbazaalain022@gmail.com>
2022-06-10 16:28:48 +0200

    feat(Authentication): correction based on PR's comments

--- src/utils/helpers/AuthToken.js
+++ src/utils/helpers/AuthToken.js
@@ -1,26 +1,22 @@
 import { status } from '../express/lib/response';
 import { sign, verify } from 'jsonwebtoken';
 import { config } from 'dotenv';
+import { stat } from 'fs';
 config();
 class AuthToken{ static TokenGenerator(data){ const token=sign({ data },
       
-        process.env.JWT_SECRET_KEY
-        ,{
+        process.env.JWT_SECRET_KEY,{
             expiresIn: '24h'
-        }
-        );
+        });
         return token;
     }
-    static decodeToken(Token){
-        try{
-            const data= verify(Token ,process.env.JWT_SECREY_KEY);
-            return data;
-        }
-        catch(error){
-            console.log(error);
-            return status(404).json({error: 'Invalid Token'});
-        }
-    }
+    static decodeToken(Token){try{const data= verify(Token ,process.env.JWT_SECRET_KEY);
+    return data;
 }
-module.exports = AuthToken;
+catch(error){console.log(error);
+return status(401).json({error: "Invalid Token"});
+}}}
+
+
+export default AuthToken;
 
