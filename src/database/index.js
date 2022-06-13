b61beab52079abb68cb597750240f06b488fa9ff
nyakamwe <nyakamweaimable@gmail.com>
2022-06-13 19:13:45 +0200

    fix(coverage): fix url for database

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,10 @@
 import { Sequelize } from "sequelize";
 import getDefault from "../helpers/getEnvironment.js";
 
-
 const env = getDefault(process.env.NODE_ENV, "development")
+console.log(env)
 
 const config = require("./config/config.js")[env];
-
-
 
 const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
