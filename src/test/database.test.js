8bf411d15c704f40e85915f6a74c89341dc8a081
nyakamwe <nyakamweaimable@gmail.com>
2022-06-13 19:49:35 +0200

    fix(coverage): describe get default helper

---
+++ src/test/database.test.js
@@ -0,0 +1,22 @@
+import chai,{expect} from 'chai';
+import ChaiHttp from 'chai-http';
+import 'dotenv/config';
+import DB from '../database';
+chai.use(ChaiHttp);
+
+describe('Database for Test connection', ()=>{
+    before(async () => {
+        // connect to database
+        await DB.authenticate();
+        
+    })
+    
+    it('test if testfoot database is connected', (done)=>{
+        expect(DB.config).to.have.property("database");
+        expect(DB.config).to.have.property("username");
+        expect(DB.config.database).to.equal(process.env.TEST_DB_NAME);
+        expect(DB.config.username).to.equal(process.env.TEST_DB_USERNAME);
+        done();
+    })
+    
+})
