3e58453a9e84f266389aa243e6ba9351192d2c0e
TetoAlia <aliateto377@gmail.com>
2022-06-15 19:16:31 +0200

    feat(login): implement login
    - added test for login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import 'core-js/stable';
-import swaggerDocs from './docs';
-import swaggerUI from 'swagger-ui-express';
+import "core-js/stable";
+import swaggerDocs from "./docs";
+import swaggerUI from "swagger-ui-express";
 import "regenerator-runtime/runtime";
 import DB from "./database/index";
 import express from "express";
 import routes from "./routes/index";
-import index from "./routes/welcome";
 import "dotenv/config";
-import getDefault from './helpers/getEnvironment'
+import getDefault from "./helpers/getEnvironment";
 
-const PORT=getDefault(process.env.PORT, "5000")
+const PORT = getDefault(process.env.PORT, "5000");
 
 // const app = express();
 
@@ -18,16 +17,16 @@ const app = express();
 // allow to parse json in body
 app.use(express.json());
 
-app.use('/api', routes);
-app.use('/', index);
+app.use("/api", routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 DB.authenticate()
-	.then(() => {
-		console.log('Database Connected');
-	})
-	.catch((err) => {
-		console.log('Database unable to connect', err);
-	});
-routes.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database unable to connect", err);
+  });
 
 app.listen(PORT, () => {
   console.log("Server has started on port", PORT);
--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	res.send({ message: 'Hello World!' });
+router.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
 });
 export default router;
