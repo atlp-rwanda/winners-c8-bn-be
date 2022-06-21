d1950f9ca4847f8fa44a93567369489b92392eb0
nyakamwe <nyakamweaimable@gmail.com>
2022-06-21 15:28:18 +0200

    documentation(swagger): add Swagger docs for acc signup and verification

--- src/docs/index.js
+++ src/docs/index.js
@@ -8,14 +8,27 @@ const swaggerOptions = {
       version: "1.0.0",
       description:
         "Welcome to Barefoot Nomad global travel and accommodation easy",
-      
+      license: {
+        name: "Licensed Under MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "ATLP8 Winners",
+        url: "https://winners-c8-bn-be-staging.herokuapp.com/",
+      },
     },
-    servers: [{
-      url: 'http://localhost:3000'
-    }],
-  },
-  apis: ["src/**/*.js"],
-};
+      servers: [
+        {
+          url:"http://localhost:5000/api"
+        },
+        {
+          url:"https://winners-c8-bn-be-staging.herokuapp.com/api"
+        },
+      ],
+    },
+    apis: ["src/**/*.js"]
+  }
+;
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -9,6 +9,41 @@ const router = Router();
 const { signup, signin, verifyUser } = authcontrollers;
 const { verifySignup, verifySignin } = AuthValidation;
 
+
+/**
+ * @openapi
+ * /auth/register:
+ *  post:
+ *      tags:
+ *          - User
+ *      description: adding the data
+ *      requestBody:
+ *          description: Add the data
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          firstName:
+ *                              type: string
+ *                          lastName:
+ *                              type: string
+ *                          password:
+ *                              type: string
+ *                          email:
+ *                              type: string
+ *      responses:
+ *          '201':
+ *              description: success response
+ *          '409':
+ *              description: user already exists
+ *          '400':
+ *              description: invalid request
+ *          '500':
+ *              description: internal server error
+ */
+
 router.post("/register", verifySignup, signup);
 /**
  * @openapi
@@ -40,5 +75,49 @@ router.post("/register", verifySignup, signup);
  *              description: internal server error
  */
 router.post("/signin", verifySignin, signin);
+
+
+/**
+ * @swagger
+ * /auth/register/verifyuser/{token}:
+ *   get:
+ *     summary: Verify a user
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - name: token
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The token of user
+ *     responses:
+ *       '400':
+ *         description: Token is invalid or expired
+ *       '201':
+ *         description: User verified successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 $ref: '#/components/schemas/VerifiedRes'
+ * components:
+ *   schemas:
+ *     VerifiedRes:
+ *       type: object
+ *       properties:
+ *         success:
+ *           type: boolean
+ *           description: Shows the updated status of verification
+ *           example: true
+ *         message:
+ *           type: string
+ *           description: Details of the results
+ *           example: User verified successfully.
+ *         data:
+ *           type: array
+ *           description: returns [1] on success and [0] on failure.
+ *           example: [1]
+ */
+
 router.get('/register/verifyuser/:token', verifyUser);
 export default router;
