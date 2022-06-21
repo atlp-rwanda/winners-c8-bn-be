16e59c26c72f56c407dd31adf50e5368cbd19c4c
nyakamwe <nyakamweaimable@gmail.com>
2022-06-21 05:28:42 +0200

    feat(user-roles-settings): resolve conflicts
    resolving conflict

--- src/middlewares/Authorization.js
+++ src/middlewares/Authorization.js
@@ -6,8 +6,7 @@ const verifyToken = async (req, res, next) => {
   const token = req.headers['x-auth-token']
   if (!token) return res.status(401).send('Access denied. No token provided!');
   try {
-    // const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
       req.user =decoded;
       next();
   }
