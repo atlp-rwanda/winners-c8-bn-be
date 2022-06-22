fed8537f075e282a8e50b5f26162cadc051d3d54
K David <kardav2003@gmail.com>
2022-06-22 11:11:14 +0200

    test(emailing): allows usage of invalid sendgrid key for tests

--- src/test/verificationEmail.test.js
+++ src/test/verificationEmail.test.js
@@ -62,4 +62,55 @@ describe("send verification email - testing", () => {
     expect(res.status).to.be.equal(400);
     expect(res.body).to.have.property("message", "Invalid or expired Token.");
   });
+  it("should successfully send the email", function (done) {
+    this.timeout(5000);
+    sendVerificationEmail("geekyrw@gmail.com").then(function (output) {
+      assert.strictEqual(typeof output, "object");
+      done();
+    });
+  });
+  it("should fail (no user email address provided)", function (done) {
+    this.timeout(5000);
+    sendVerificationEmail().then(function (output) {
+      assert(output.hasOwnProperty("error"));
+      done();
+    });
+  });
+  it("it should send email on a successful registration", async () => {
+    const res = await chai.request(app).post("/api/auth/register").send(signup);
+    expect(res.status).to.be.equal(201);
+    authTokenTest = res.body.data;
+    expect(res.body).to.have.property(
+      "message",
+      "User registered successfully"
+    );
+  });
+  it("it should verify user in the database", async () => {
+    const res = await chai
+      .request(app)
+      .get("/api/auth/register/verifyuser/" + authTokenTest)
+      .send();
+
+    expect(res.status).to.be.equal(201);
+    expect(res.body).to.have.property("message", "User verified successfully");
+  });
+  it("it can not verify non-existing user", async () => {
+    await User.destroy({ where: {} });
+    const res = await chai
+      .request(app)
+      .get("/api/auth/register/verifyuser/" + authTokenTest)
+      .send();
+
+    expect(res.status).to.be.equal(409);
+    expect(res.body).to.have.property("message", "Ooops! User does not exist!");
+  });
+  it("it can not verify with invalid token", async () => {
+    const res = await chai
+      .request(app)
+      .get("/api/auth/register/verifyuser/" + "122ffee")
+      .send();
+
+    expect(res.status).to.be.equal(400);
+    expect(res.body).to.have.property("message", "Invalid or expired Token.");
+  });
 });
