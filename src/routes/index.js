568d0e084886cfa3c7ef82c0270ddf1a878e7a24
nyakamwe <nyakamweaimable@gmail.com>
2022-06-22 11:47:52 +0200

    feat(user-roles-settings): retouch from rebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ import auth from './Auth';
 import isAuthenticated from '../middlewares/Authorization';
 import userRoutes from './usersRoutes'
 
-import userRoutes from './usersRoutes'
-
 const router = Router();
 
 router.get("/users",isAuthenticated, async (req, res) => {
