e452dba35e5960e024b2cf1b5c9daa2a1efa8bb4
K David <kardav2003@gmail.com>
2022-06-22 10:07:45 +0200

    Added productuin mode to heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,17 +1,18 @@
 require("dotenv").config();
+//In .env use in development config env file
+//DATABASE_URL=postgres://username:password@localhost:5432/database_name
+//postgres://ihimbazwe:elissa123@localhost:5432/Development
 
 module.exports = {
-
   development: {
     use_env_variable: "DATABASE_URL_DEV",
-    dialect: 'postgres',
+    dialect: "postgres",
   },
   test: {
     use_env_variable: "DATABASE_URL_TEST",
-    dialect: 'postgres',
+    dialect: "postgres",
   },
   production: {
-    dialect: 'postgres',
     use_env_variable: "DATABASE_URL",
   },
 };
