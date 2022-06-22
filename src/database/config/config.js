9207fb98fc13783321eab87d1109db19110464ce
K David <kardav2003@gmail.com>
2022-06-22 10:14:27 +0200

    Added  Fixed babel config  dependencies  to heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -11,11 +11,11 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: "postgres",
+    // username: process.env.PROD_DB_USERNAME,
+    // password: process.env.PROD_DB_PASSWORD,
+    // database: process.env.PROD_DB_NAME,
+    // host: process.env.PROD_DB_HOSTNAME,
+    // port: process.env.PROD_DB_PORT,
+    // dialect: "postgres",
   },
 };
