3319e169d0b040119fb2196136cf8d1e124918ea
alain-honore <himbazaalain022@gmail.com>
2022-06-22 16:46:12 +0200

    fix(heroku): connect the app to heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,4 +14,10 @@ module.exports = {
     dialect: 'postgres',
     use_env_variable: "DATABASE_URL",
   },
+  dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+}
+}
 };
