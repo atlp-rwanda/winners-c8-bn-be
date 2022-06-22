3a35bd71b13aa625eab51e5f8af02ed7d133d287
K David <kardav2003@gmail.com>
2022-06-22 10:15:10 +0200

    fix(Conflicting files): removed the cors in index and packageloc

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import swaggerUI from "swagger-ui-express";
 import DB from "./database/index";
 import express from "express";
 import routes from "./routes/index";
-import cors from "cors";
 
 // connecting to database
 DB.authenticate().then(() => {
@@ -14,7 +13,6 @@ DB.authenticate().then(() => {
 const PORT = getDefault(process.env.PORT, "5000");
 
 const app = express();
-app.use(cors());
 
 // allow to parse json in body
 app.use(express.json());
