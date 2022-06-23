6790a608cf0a22514b370ec7c161c1a29a50ed91
alain-honore <himbazaalain022@gmail.com>
2022-06-23 13:03:22 +0200

    fix(heroku): fixing heroku for testing app

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,11 +13,12 @@ module.exports = {
   production: {
     dialect: 'postgres',
     use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+    }
   },
-  dialectOptions: {
-  ssl: {
-    require: true,
-    rejectUnauthorized: false
-}
-}
+
 };
