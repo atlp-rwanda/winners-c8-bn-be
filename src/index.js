d72cc0cf7711cf8773d57564ab6c01cefdab5847
nyakamwe <nyakamweaimable@gmail.com>
2022-06-24 14:24:21 +0200

    work on chat

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import express from "express";
 import routes from "./routes/index";
 import "dotenv/config";
 import getDefault from "./helpers/getEnvironment";
+import socket from "socket.io";
+import path from "path";
 
 const PORT = getDefault(process.env.PORT, "5000");
 
@@ -23,8 +25,17 @@ DB.authenticate().then(() => {
   console.log("Database Connected");
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log("Server has started on port", PORT);
 });
 
+// Socket setup
+const io = socket(server);
+io.on('connection', socket=>{
+  console.log('socket connected')
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/v1/users/chats', (req,res)=>{
+  res.sendFile(path.join(`${__dirname}/public/chat.html`));
+})
 export default app;
---
+++ src/public/chat.js
@@ -0,0 +1,39 @@
+// setup socket on client/ browser by using io object from linked cdn
+
+const socket = io.connect("http://localhost:5000/api/v1/users/chats");
+
+const senderName = document.querySelector('#name');
+const message = document.querySelector('#message');
+const output = document.querySelector('#output');
+const sendBtn = document.querySelector('#send');
+const feedback = document.querySelector("#feedback")
+
+sendBtn.addEventListener('click', ()=>{
+
+    //Emit or send chat to the server
+    socket.emit('chat', {
+        message:message.value,
+        name:senderName.value
+    })
+
+
+})
+
+//event for keypress on message
+message.addEventListener('keypress', function(){
+
+    socket.on('typing', senderName.value)
+
+
+})
+
+// Listening or receiving to message sent by server
+
+socket.on('chat', function(data){
+    feedback.innerHTML =  "";
+    output.innerHTML += "<p><em> "+data.name +": </em> " +data.message + "</p>"
+})
+
+socket.on('typing',function(data){
+    feedback.innerHTML = "<p> " + data + "is typing..." + "</p>" 
+})
