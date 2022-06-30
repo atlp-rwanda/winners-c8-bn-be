7a68f30aa091e450f0a8abbcd242c4c7b7091de1
Ihimbazwe Elissa <ihimbazwelisa@gmail.com>
2022-06-30 22:04:42 +0200

    chore(mail): removed uud in the token string

--- src/helpers/sendResetEmail.js
+++ src/helpers/sendResetEmail.js
@@ -2,19 +2,16 @@ import mail from '@sendgrid/mail';
 import 'dotenv/config';
 
 
-import { v4 as uuidv4 } from 'uuid';
-
 
 //Sending Password reset email
 const sendResetEmail = async (user, redirectUrl, res, token) =>{
-    const resetString = uuidv4() ;
     mail.setApiKey(process.env.SENDGRID_API_KEY);
 
     const mailData = {
         from: process.env.TECH_SUPPORT_EMAIL,
         to: user.email,
         subject: 'Barefoot Nomad Password reset Link',
-        html: `We heard you lost your password <b> <a href=${ redirectUrl + "/" + token + "/" + resetString}>Click here</a> </b> to procced`
+        html: `We heard you lost your password <b> <a href=${ redirectUrl + "/" + token}>Click here</a> </b> to procced`
       };
     
       return mail.send(mailData)
